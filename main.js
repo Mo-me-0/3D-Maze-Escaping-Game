7a8bcd1d85882b0ae26edfb2d3650dd91b50cef9
Mussie Workneh <156955588+Mo-me-0@users.noreply.github.com>
2025-12-01 23:21:37 +0300

    initial commit
    basic scene

---
+++ main.js
@@ -0,0 +1,16 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate(){
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+
+
